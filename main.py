24330ebd21dc0d911ee8f334b9b1a06421dcde53
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-05 15:40:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ autocaption = Client(
                       
 @autocaption.on_message(filters.command("start") & filters.incoming)
 async def start(bot, message):
-        await message.reply("<I>I Am Auto Caption Bot Just Add me as a Admin in your channel with edit permission and See Magic</I>\n\n• **support** : @Hollywodd_0980\n• **Source** : https://github.com/0AIB/TG-Caption-Bot")
+        await message.reply("<I>I Am Auto Caption Bot Just Add me as a Admin in your channel with edit permission and See Magic</I>\n\n• **support** : @Hollywood_0980\n• **Source** : https://github.com/0AIB/TG-Caption-Bot")
 
 @autocaption.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
 async def editing(bot, message):
