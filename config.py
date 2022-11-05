d956971cd864df0edd612c60b41525947715acea
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-05 12:32:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config(object):
       API_ID = int(os.environ.get("APP_ID", 12345))
       API_HASH = os.environ.get("API_HASH")
       CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "CvSubhan")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
+      
